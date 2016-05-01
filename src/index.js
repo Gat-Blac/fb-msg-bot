8a6ef919bfe240711108f02c3b12cf7817c2e3a7
ElMati <matias.uribe.v@gmail.com>
2016-04-30 23:00:00 -0300

    se agrega mensaje generico y obtener profile

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ util.inherits(FacebookMsgBot, EventEmitter);
 * @param {String} text
 */
 FacebookMsgBot.prototype.sendTextMessage = function(sender, text, callback){
-    messageData = {
+    var messageData = {
      'text':text
    }
    request({
@@ -42,3 +42,54 @@ FacebookMsgBot.prototype.sendTextMessage = function(sender, text, callback){
      }
    });
 }
+
+/**
+* Generates message with template.
+* @param {String} sender
+* @param {String} messageData
+*/
+
+FacebookMsgBot.prototype.sendGenericMessage = function(sender, messageData, callback) {
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:this.token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      callback(error,null);
+    } else if (response.body.error) {
+      callback(response.body.error,null);
+    } else {
+      callback(null,response.body);
+    }
+  });
+}
+
+/**
+* Get information of the user.
+* @param {String} id
+*/
+FacebookMsgBot.prototype.getProfile = function(id, isJson, callback) {
+    request({
+      method: 'GET',
+      url: 'https://graph.facebook.com/v2.6/' + id,
+      qs: {
+        access_token: this.token
+      },
+      json: isJson
+    }, function(error, response, body) {
+      if (error) {
+        callback(error,null);
+      } else if (body.error) {
+        callback(response.body.error,null);
+      } else {
+        callback(null,response.body);
+      }
+    })
+  }
+
+module.exports = FacebookMsgBot;
